4ec5d383a412e1614445a50e21b35a86923b0ccf
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-02-23 13:41:46 +0100

    icons

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { RiDeleteBack2Fill } from "react-icons/ri";
 
 const TaskList = ({ task, getTask }) => {
-  return <div>TaskList</div>;
+  return (
+    <div>
+      {task.map((item) => {
+        const { id, task, date } = item;
+        return (
+          <div>
+            <div>
+              <h4>{task}</h4>
+              <p>{date}</p>
+            </div>
+            <div>
+              <RiDeleteBack2Fill
+                style={{
+                  cursor: "pointer",
+                }}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default TaskList;
